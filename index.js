3cdd2a63600168d4528827e78cdfa61be3694414
raktimproloy <raktimproloy@gmail.com>
2023-09-14 23:03:02 +0600

    added database

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.BASE_URL)
+    .then(() => console.log("Database Connected successful"))
+    .catch(err => console.log(err.message))
 
 app.listen(3000, () => {
     console.log(`Server 3000`)
